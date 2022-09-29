9cbcce105a37d824d660cb6e92ebb1e465a4de7d
yummy007 <bartoszkaam@gmail.com>
2022-09-29 12:02:21 +0200

    .

---
+++ zadania.py
@@ -0,0 +1,17 @@
+
+# a = "hog rider"
+# print(a)
+
+# b=100.1
+# print(b)
+# simeaijfbaijbf= 109.14091
+# print(simeaijfbaijbf)  
+
+
+a = str(input("siema stary, potrzenujemy cie w mnaszym skladzie, pomozesz nam? : "))
+
+
+if a : "tak" 
+    print ("dziena")
+else:
+    print("nie jsestes mily")
